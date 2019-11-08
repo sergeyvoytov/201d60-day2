3caf2d046f545c3478de785a597871d08ae782da
jjdeforrest <jjdeforrest@users.noreply.github.com>
2019-11-07 21:01:31 -0800

    functions

--- js/app.js
+++ js/app.js
@@ -1,14 +1,13 @@
 'use strict';
 //Find out the name of the user and greetingm
-//dsfd
+fucntion greeting (){
 var userName = prompt('Hello! What is your name?');
 alert('Good to meet you ' + userName + '! ' + 'Please answer few questions about Sergey to learn more about him in Yes/No format.');
-
+}
 //First question about dogs with uppercase function
 function questionOne(){
 var dogs = prompt('Does Sergey like dogs?').toUpperCase();
 console.log('likes dogs:', dogs);
-
 if (dogs === 'YES') {
   //console.log('You are correct! Sergey likes dogs');
   alert('You are correct! Sergey likes dogs');
@@ -18,6 +17,7 @@ if (dogs === 'YES') {
 }
 }
 //Second question about piano with uppercase function
+function questionTwo (){
 var playsPiano = prompt('Does Sergey play piano?').toUpperCase();
 console.log('plays piano:', playsPiano);
 
@@ -28,8 +28,9 @@ if (playsPiano === 'YES') {
   //console.log('You are correct! Sergey doesn\'t play piano');
   alert('You are correct! Sergey doesn\'t play piano');
 }
-
+}
 //Third question about life in Seattle
+function questionThree () {
 var lifeSeattle = prompt('Does Sergey live in Seattle?').toUpperCase();
 console.log('lifeSeattle:', lifeSeattle);
 
@@ -40,9 +41,9 @@ if (lifeSeattle === 'YES') {
   //console.log('You are incorrect. Sergey lives in Seattle');
   alert('You are incorrect. Sergey lives in Seattle');
 }
-
+}
 // Forth question
-
+function questionFour (){
 var playsSoccer = prompt('Does Sergey like soccer?').toUpperCase();
 console.log('playsSoccer:', playsSoccer);
 
@@ -53,9 +54,9 @@ if (playsSoccer === 'YES') {
   //console.log('You are incorrect. Sergey likes soccer');
   alert('You are incorrect. Sergey likes soccer');
 }
-
+}
 // Fifth question
-
+funciton questionFive (){
 var classicalMusic = prompt('Does Sergey like classical music?').toUpperCase();
 console.log('classicalMusic:', classicalMusic);
 
@@ -67,9 +68,10 @@ if (classicalMusic === 'YES') {
   alert('You are correct! Sergey doesn\'t like classical music');
 }
 alert(userName + ' , thank you for using my site and learning about me!');
-
+}
 
 // Sixth question
+function questionSix (){
 var number;
 var counter = 0;
 
@@ -91,9 +93,9 @@ while (counter !== 4) {
 if (counter === 4) {
   alert('you have reached your attempts. Right answer was 20');
 }
-
+}
 // seventh question
-
+function questionSeven (){
 var food = ['beer','pizza','cheese','chicken','apple', 'smoked fish','cookies'];
 
 for (var i=0; i < 7; i++){
@@ -105,3 +107,13 @@ break;
 alert('Try again please');
 }
 }
+}
+
+function greeting ();
+function questionOne ();
+function questionTwo ();
+function questionThree ();
+function questionFour ();
+function questionFive ();
+function questionSix ();
+function questionSeven ();
